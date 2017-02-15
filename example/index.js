e6ec4af3f4e9224c4e8f3761700baef170b503cd
Ionică Bizău <bizauionica@gmail.com>
2017-02-15 17:35:23 +0200

    Example

--- example/index.js
+++ example/index.js
@@ -2,4 +2,31 @@
 
 const gitUnsaved = require("../lib");
 
-console.log(gitUnsaved());
+gitUnsaved(`${__dirname}/..`, (err, data) => {
+
+    if (err) {
+        // Something went really wrong
+        return;
+    }
+
+    console.log(data);
+    // { result: { branch: 'master', ahead: 4, dirty: 3, untracked: 0, stashes: 0 },
+    //   messages:
+    //    [ 'You have \u001b[1m4\u001b[22m \u001b[1munpushed commits\u001b[22m.',
+    //      'You have \u001b[1m3\u001b[22m \u001b[1mfiles you have to commit\u001b[22m.' ],
+    //   path: '/home/.../git-unsaved' }
+
+    data.messages.forEach(c => {
+        console.log(`  ${c}`);
+    });
+
+    // =>
+    // You have 4 unpushed commits.
+    // You have 3 files you have to commit.
+    console.log();
+}).on("directory", path => {
+    console.log(`Scanning ${path}`)
+    // Scanning /home/.../git-unsaved/...
+}).on("end", () => {
+    console.log("Done")
+});
